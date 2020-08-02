0fd84e0fe491d9995ac471fd76827915c47c6fb0
Omarmok98 <omarmok@icloud.com>
2020-08-02 21:18:06 +0200

    Updated Models

---
+++ lib/models/BloodGlucose.dart
@@ -0,0 +1,33 @@
+enum TimeType { fasting, afterEating, two_three_hours_aftrer_eating }
+
+class BloodGlucose {
+  String id;
+  int value;
+  TimeType timeType;
+  String note;
+  DateTime date;
+
+  BloodGlucose({this.id, this.value, this.date, this.timeType, this.note});
+
+  factory BloodGlucose.fromJson(Map<String, dynamic> json) {
+    return BloodGlucose(
+      id: json["_id"],
+      value: json["value"],
+      timeType: json["type"],
+      note: json["note"],
+      date: json['createdAt'] != null
+          ? DateTime.parse(json['createdAt'])
+          : DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "_id": this.id,
+      "value": this.value,
+      "type": this.timeType,
+      "note": this.note,
+      "date": this.date
+    };
+  }
+}
