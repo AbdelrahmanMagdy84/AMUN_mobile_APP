0e5df4e008c5b97e909bc80d01733608a289189b
Omarmok98 <omarmok@icloud.com>
2020-08-08 19:11:53 +0200

    Added Medical record services

--- lib/models/MedicalRecord.dart
+++ lib/models/MedicalRecord.dart
@@ -1,11 +1,80 @@
+import 'Clerk.dart';
+import 'Doctor.dart';
+import 'MedicalFacility.dart';
+
 class MedicalRecord {
-  int id;
+  String id;
   DateTime date;
   String title;
-  String doctor;
-  String image;
   String note;
-
+  String enteredBy;
+  MedicalFile report;
+  MedicalFile radiograph;
+  MedicalFile prescription;
+  Clerk clerk;
+  Doctor doctor;
+  MedicalFacility medicalFacility;
   MedicalRecord(
-      {this.id, this.title, this.doctor, this.date, this.image, this.note});
+      {this.id,
+      this.title,
+      this.date,
+      this.note,
+      this.enteredBy,
+      this.prescription,
+      this.radiograph,
+      this.report,
+      this.clerk,
+      this.doctor,
+      this.medicalFacility});
+
+  factory MedicalRecord.fromJson(Map<String, dynamic> json) {
+    //for get requests
+    return MedicalRecord(
+      id: json["_id"],
+      title: json["value"],
+      enteredBy: json['enteredBy'],
+      note: json["note"],
+      date:
+          json['date'] != null ? DateTime.parse(json['date']) : DateTime.now(),
+      report: json['report'].fromJson(),
+      radiograph: json['radiograph'].fromJson(),
+      prescription: json['prescriptionImage'].fromJson(),
+      clerk: json['clerk'].fromJson(),
+      doctor: json['doctor'].fromJson(),
+      medicalFacility: json['medicalFacility'].fromJson(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    //for post requests
+    return {
+      "title": this.title,
+      "note": this.note,
+      "enteredBy": this.enteredBy,
+      "date": this.date.toIso8601String(),
+      "report": this.report.toJson(),
+      "radiograph": this.radiograph.toJson(),
+      "prescriptionImage": this.prescription.toJson()
+    };
+  }
+}
+
+class MedicalFile {
+  String url;
+  String fileName;
+
+  MedicalFile({this.url, this.fileName});
+  factory MedicalFile.fromJson(Map<String, dynamic> json) {
+    return MedicalFile(
+      url: json["url"],
+      fileName: json["fileName"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "url": this.url,
+      "fileName": this.fileName,
+    };
+  }
 }
---
+++ lib/models/Responses/MedicalRecordsResponse.dart
@@ -0,0 +1,25 @@
+import '../MedicalRecord.dart';
+
+class MedicalRecordsResponse {
+  String message;
+  bool success;
+  List<MedicalRecord> medicalRecord;
+
+  MedicalRecordsResponse({
+    this.medicalRecord,
+    this.message,
+    this.success,
+  });
+
+  factory MedicalRecordsResponse.fromJson(Map<String, dynamic> json) {
+    List<dynamic> parsedMedicalRecord = json['medicalRecord'] != null
+        ? json['medicalRecord']
+        : new List<dynamic>();
+    List<MedicalRecord> record =
+        parsedMedicalRecord.map((i) => MedicalRecord.fromJson(i)).toList();
+    return MedicalRecordsResponse(
+        success: json["success"],
+        message: json["message"],
+        medicalRecord: record);
+  }
+}
