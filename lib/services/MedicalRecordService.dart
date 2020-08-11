e555c63e6c6e54c56c52a68b9ff2d75ecded305e
Omarmok98 <omarmok@icloud.com>
2020-08-11 19:33:04 +0200

    added FacilityPatientService

--- lib/services/MedicalRecordService.dart
+++ lib/services/MedicalRecordService.dart
@@ -31,39 +31,37 @@ class MedicalRecordService {
 
   Future<MedicalRecordResponse> addMedicalRecords(
       MedicalRecord medicalRecord, String token, String type) async {
-    /* final http.Response response = await http.post(
-        "${APIClient.baseUrl}/$endPoint",
-        headers: {"Content-Type": "application/json", "authorization": token},
-        body: jsonEncode(medicalRecord.toJson(type)));
-    print(jsonDecode(response.body)); */
-    print("request begins");
     Map<String, String> headers = {"authorization": token};
-    print("headers intializaed");
     var request = http.MultipartRequest(
         'POST', Uri.parse("${APIClient.baseUrl}/$endPoint"));
-    print("assigning header");
     request.headers.addAll(headers);
-    print("assigning fields");
     request.fields['date'] = medicalRecord.date.toIso8601String();
     request.fields['notes'] = medicalRecord.note;
     request.fields['enteredBy'] = medicalRecord.enteredBy;
     request.fields['title'] = medicalRecord.title;
     request.fields['type'] = medicalRecord.type;
-    print("assigning files");
     request.files
         .add(await http.MultipartFile.fromPath(type, medicalRecord.filePath));
-    print("sending requests");
     print(request.fields);
     var streamedResponse = await request.send();
-    print("response received");
     var response = await http.Response.fromStream(streamedResponse);
-    print("response converted");
     print(jsonDecode(response.body));
     if (response.statusCode == 200) {
       print("success");
       return MedicalRecordResponse.fromJson(jsonDecode(response.body));
     } else {
       throw Exception("Failed to post data");
+    }
+  }
+
+  Future<void> deleteMedicalRecord(String id, String token) async {
+    final http.Response response = await http.delete(
+        "${APIClient.baseUrl}/$endPoint/$id",
+        headers: {"Content-Type": "application/json", "authorization": token});
+    if (response.statusCode == 200) {
+      return MedicalRecordsResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to delete data");
     }
   }
 }
