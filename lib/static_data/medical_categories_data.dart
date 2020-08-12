d79b90575e3d28015b4750f84b2fd3484705def1
AbdelrahmanMagdy84 <45487306+AbdelrahmanMagdy84@users.noreply.github.com>
2020-08-12 23:48:01 +0200

    edit categories

--- lib/static_data/medical_categories_data.dart
+++ lib/static_data/medical_categories_data.dart
@@ -9,9 +9,9 @@ class Category {
 
 List<Category> categories = [
   Category(id: '1', title: "Blood Glocose", image: 'assets/images/glocose.jpg'),
-  Category(id: '2', title: "Blood Pressure", image: 'assets/images/pressure.jpg'),
+  Category(
+      id: '2', title: "Blood Pressure", image: 'assets/images/pressure.jpg'),
   Category(id: '3', title: "Prescription", image: 'assets/images/pres.png'),
-  Category(id: '4', title: "Reminders", image: 'assets/images/appointment.png'),
-  Category(id: '5', title: "Radiograph", image: 'assets/images/radiograph.png'),
-  Category(id: '6', title: "Lab Test", image: 'assets/images/lab.png'),
+  Category(id: '4', title: "Radiograph", image: 'assets/images/radiograph.png'),
+  Category(id: '5', title: "Lab Test", image: 'assets/images/lab.png'),
 ];
