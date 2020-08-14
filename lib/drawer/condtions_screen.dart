23beb50e07ef239807189bd25fbc93502cd651b5
AbdelrahmanMagdy84 <45487306+AbdelrahmanMagdy84@users.noreply.github.com>
2020-08-14 03:14:41 +0200

    editing of interface

--- lib/drawer/condtions_screen.dart
+++ lib/drawer/condtions_screen.dart
@@ -89,15 +89,15 @@ class _ConditionsScreenState extends State<ConditionsScreen> {
       ),
       body: Container(
         padding: EdgeInsets.only(left: 10, right: 10, top: 20),
-        child: conditions == null
+        child: conditions == []
             ? Center(
                 child: Text("Empty press + to add"),
               )
             : GridView.count(
                 crossAxisCount: 2,
                 childAspectRatio: 1 / 1,
-                crossAxisSpacing: MediaQuery.of(context).size.width * 0.05,
-                mainAxisSpacing: MediaQuery.of(context).size.height * 0.05,
+                crossAxisSpacing: MediaQuery.of(context).size.width * 0.001,
+                mainAxisSpacing: MediaQuery.of(context).size.height * 0.01,
                 children: List.generate(reversedList.length, (index) {
                   return buildItem(reversedList[index]);
                 })),
@@ -115,44 +115,46 @@ class _ConditionsScreenState extends State<ConditionsScreen> {
   }
 
   Widget buildItem(String condition) {
-    return Container(
+    return Card(
       child: LayoutBuilder(
         builder: (ctx, constraints) {
           return Container(
             child: Container(
               decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Theme.of(context).primaryColor.withOpacity(.2),
-                    Theme.of(context).primaryColor
-                  ], //here you can change the color
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                ),
-                borderRadius: BorderRadius.circular(40),
+                color: Theme.of(context).accentColor,
+                //  borderRadius: BorderRadius.circular(40),
               ),
               child: Container(
-                padding: const EdgeInsets.only(top: 10),
+                margin: const EdgeInsets.only(top: 0),
                 child: Container(
                   width: constraints.maxWidth * 0.8,
-                  padding: EdgeInsets.only(top: 5, left: 5),
+                  //    padding: EdgeInsets.only(top: 5, left: 5),
                   child: Column(
                     children: <Widget>[
-                      Text(
-                        'Condition: ',
-                        style: Theme.of(context).textTheme.title,
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 10, top: 10),
+                        width: double.infinity,
+                        color: Colors.amber,
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Conditions: ',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                       Divider(
-                        color: Theme.of(context).accentColor,
-                        thickness: 0.6,
-                      ),
-                      Center(
-                        child: Container(
-                          padding: EdgeInsets.all(5),
-                          child: Text(
-                            '$condition',
-                            maxLines: 5,
-                            style: TextStyle(fontSize: 18),
+                      Container(
+                        height: 100,
+                        child: Center(
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            child: Text(
+                              '$condition',
+                              maxLines: 5,
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.white),
+                            ),
                           ),
                         ),
                       ),
